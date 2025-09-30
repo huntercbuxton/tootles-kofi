1f47307fc132730c82d6530bc4ae7b5fbb28417b
Hunter Buxton <hunter.buxton@protonmail.com>
2025-09-30 01:28:54 -0600

    add tests

---
+++ src/components/KofiShop/__test__/KofiShopCard.test.tsx
@@ -0,0 +1,12 @@
+//KofiShop/__test__/KofiShopCard.test.tsx 
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { KofiShopCard } from "../KofiShop";
+
+describe("KofiShopCard", () => {
+  it("card should render correctly", () => {
+    render(<KofiShopCard Alias={"aaaaaa"}  Name={"kofi shop card example"}  Price={"100.00"} ThumbnailUrls={["aaaaaa"]} Description={"placeholder"} />);
+    const card = screen.getByText("kofi shop card example");
+    expect(card).toBeInTheDocument();
+  });
+});
---
+++ src/components/KofiShop/__test__/KofiShopGrid.test.tsx
@@ -0,0 +1,12 @@
+//KofiShop/__test__/KofiShopGrid.test.tsx
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { KofiShopGrid } from "../KofiShop";
+ 
+describe("KofiShopGrid", () => {
+  it("grid should render correctly", () => {
+   render(<KofiShopGrid inventory={ [ { Alias: "aaaaaa",  Name:"kofi shop card example", Price: "100.00" , ThumbnailUrls: ["asdasdad"], Description: "placeholder" } ] }    />);
+    const grid = screen.getByText("kofi shop card example");
+    expect(grid).toBeInTheDocument();
+  });
+});
